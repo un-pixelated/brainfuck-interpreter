16948f8f5e24509bd0400ff52a5469c7ef1da90b
Hardik Chona <masterhc321@gmail.com>
2026-02-22 18:37:59 +0530

    Finished stack.h and noted down future pans in archPlan.txt

--- stack.h
+++ stack.h
@@ -1,17 +1,26 @@
 #ifndef STACK_H
 #define STACK_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "constants.h"
 
 struct Stack {
-        int data[STACK_SIZE];
+        int array[STACK_SIZE];
         int top;
 };
 
 typedef struct Stack Stack;
 
-Stack *init_stack(void) {
+Stack *stack_init(void) {
         Stack *stack = (Stack *) malloc(sizeof(Stack));
+
+        if (stack == NULL) {
+                fprintf(stderr, "couldn't initialize stack\n");
+                return NULL;
+        }
+
         stack->top = -1;
         return stack;
 }
@@ -24,6 +33,26 @@ int stack_empty(Stack *stack) {
 int stack_full(Stack *stack) {
         if (stack->top == STACK_SIZE - 1) return 1;
         return 0;
+}
+
+void stack_push(Stack *stack, int data) {
+        if (stack_full(stack) == 1) {
+                fprintf(stderr, "stack overflow\n");
+                return;
+        }
+
+        stack->top++;
+        stack->array[stack->top] = data;
+}
+
+int stack_pop(Stack *stack) {
+        if (stack_empty(stack) == 1) {
+                fprintf(stderr, "stack underflow\n");
+                return -1;
+        }
+
+        stack->top--;
+        return stack->array[stack->top + 1];
 }
 
 #endif
