0178248ff1118ae2513388d720f9fda505fa0839
Hardik Chona <masterhc321@gmail.com>
2026-02-25 23:24:07 +0530

    [RF] Changed MAP_LEN from 100,000 to 30,000 to be consistent with CELL_ARRAY_LENGTH (just a personal design choice), explained MAP_LEN's purpose clearly in a comment, and moved CELL_SIZE's define above MAP_LEN's for better looking code

--- headers/constants.h
+++ headers/constants.h
@@ -4,11 +4,12 @@
 // length of the cell_array
 #define CELL_ARRAY_LENGTH 30000
 
-// length of the bracket validation map (ideally length of file)
-#define MAP_LEN 100000
-
 // size of a cell in bytes
 #define CELL_SIZE 1
+
+// length of the bracket validation map (ideally length of file)
+// this limits the length of the file you can throw into the interpreter
+#define MAP_LEN 30000
 
 // size for the parentheses matching stack (nesting depth)
 #define STACK_SIZE 1024
